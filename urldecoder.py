fe48a6bb5ec980834054f747b4c0b123e0f239fb
Parris Mook-Sang-Forbes <parris@validmind.ai>
2025-05-21 12:28:39 -0700

    Updated documentation

--- urldecoder.py
+++ urldecoder.py
@@ -1,10 +1,11 @@
-from urllib.parse import unquote
-import re
-import base64
-import requests
-import shutil #picked up from https://stackoverflow.com/a/18043472
+from urllib.parse import unquote #used to undo JavaScript URL encoding
+import re #used to identify & isolate different parts of a URL
+import base64 #used to decode/encode filenames associated with strip images
+import requests #used to GET strip images from the Comics Kingdom website
+import shutil #used to write GET response into .jpg files
 
-#it's easiest just to change the URL in the code itself since I'm making this for myself.
+#This script works by guessing at a range of numbers associated with a strip image URL, which changes only once per week. Therefore,
+#I think it makes more sense to define the encoded URL as a global variable, rather than supplying it as a command line input.
 ENCODED_URL = "https://comicskingdom.com/_next/image?url=https%3A%2F%2Fwp.comicskingdom.com%2Fcomicskingdom-redesign-uploads-production%2F2025%2F05%2FY2tGbGFzaCBHb3Jkb24tRU5HLTUxMTU0MTU.jpg&w=3840&q=75"
 RANGE = 12 #establishing how wide a range we want to set on either side of our link fishing scheme. 12 seems to be wide enough.
 COMIC_PREFIX = "ckFlash Gordon-ENG-" #the prefix of the comic one wants to download--in this case, it's the prefix for Flash Gordon.
@@ -27,7 +28,7 @@ def get_image_name(url):
     '''
     Gets the filename for the image stored at the URL.
     Though slicing off the file suffix makes decoding
-    from BASE64 easier, it means adding '.jpeg' back on the
+    from BASE64 easier, it means adding '.jpg' back on the
     end when we want to actually retrieve the file.
     '''
     imageName = re.search("/[0-9]{2}/.+.jpg$", url).group(0)[4:-4]
@@ -51,18 +52,23 @@ def get_image_number(imageName):
 
 def replace_image_name(oldImageName, newImageName, url):
     '''
-    TODO: document
+    Given a URL linking to a comic strip, replaces the old image name
+    in the URL with a new image name before returning the new URL.
+    Note that the new image name may not actually link to an image, and
+    that the URL is always expected to be the location of a jpeg image.
     '''
     startIndex = url.find(oldImageName)
-    #print("new url is:", url)
     url = url[:startIndex] + newImageName + ".jpg"
-    #print("old url is:", url)
     return url
 
 
 def create_url(imageName, newImageNumber, url):
     '''
-    TODO: document
+    Given a even digit image number, creates an image name using the
+    number as a suffix (following the "ckFlash Gordon-ENG-xxxxxxx" format),
+    encodes the image name in BASE64, and adds a ".jpg" file suffix to create
+    a filename. A new URL is created based on the URL provided, with
+    the newly created filename in place of the old one.
     '''
     imageNameDecoded = COMIC_PREFIX + str(newImageNumber)
     imageNameBytes = imageNameDecoded.encode("ascii")
@@ -78,19 +84,34 @@ def get_image(url, imageName):
     and stores the raw socket response (stream=True). Next, shutil's copyfileobj
     method copies the contents of this file-like object (the raw response) into
     an actual file (filename.jpg, where filename is the original filename of the
-    .jpg stored at the provided address)
-    If there is no image stored at the provided address, function returns. (is there a better way to phrase that?)
+    .jpg stored at the provided address) However, if there is no image at the
+    provided URL, no attempt to download is made.
+
+    I learned of shutil--and borrowed this particular implimentation--from
+    this stack overflow post: https://stackoverflow.com/a/18043472
     '''
+
     response = requests.get(url, stream=True)
     if response.status_code == 200:
-        with open(imageName + '.jpg', 'wb') as out_file: #i know it's dumb to add .jpg back to the end after removing it--cut me some slack, alright?
+        with open(imageName + '.jpg', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
-    del response #deleting request/filelike object
+    del response
 
 
 def get_week(imageName, imageNumber, url):
     '''
-    TODO: document
+    This is the 'guessing' function. When provided the image number associated with the
+    encoded URL's Flash Gordon strip, it guesses at possible image numbers corresponding
+    to the rest of the week's strips. Guesses are made within a set range, provided by the
+    RANGE variable. Strip numbers are set at increments of two, so guesses are also made
+    in increments of two. These strip numbers are not linear--that is, they do not necessarily
+    grow larger or smaller throughout the week. They are randomized within this certain range.
+    
+    Sunday strips fall outside the range of daily strips, and the week's range changes each week.
+    Therefore, this script is at its most useful during the beginning of the week (when it can
+    retrieve five new strips) than at the end (where the six retrieved strips have already been
+    publically released). However, one can access next week's strip range by copying the URL of
+    the image which is part of a Comics Kingdom embed.
     '''
     i = 2
     while (i <= RANGE):
@@ -113,9 +134,6 @@ def get_week(imageName, imageNumber, url):
 
 
 def main ():
-    '''
-    TODO: write some documentation lol.
-    '''
     url = reformat_url()
     imageName = get_image_name(url)
     imageNumber = get_image_number(imageName)
